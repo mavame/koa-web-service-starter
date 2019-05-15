b960f01f9e789b147ba2deb737f9520afe8a9391
Matt Van Meter <matt.vanmeter@dynamit.com>
2019-05-15 17:09:24 -0400

    Adds objection

---
+++ server/database.js
@@ -0,0 +1,11 @@
+const { env } = require("./config");
+const knex = require("knex");
+const knexConfig = require("../knexfile");
+const { Model } = require("objection");
+
+// set up the database connection and ORM
+Model.knex(knex(knexConfig[env]));
+
+class BaseModel extends Model { }
+
+module.exports = { knex, Model, BaseModel };
