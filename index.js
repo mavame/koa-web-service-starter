cbc4cbd75aa5b016821c1e384b91359e99edf138
Matt Van Meter <matt.vanmeter@dynamit.com>
2019-05-15 16:16:02 -0400

    Initial commit; includes koa and nodemon for auto-restarting.

---
+++ index.js
@@ -0,0 +1,12 @@
+const Koa = require("koa");
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve('Hello world!');
+    }, 5000);
+  });
+});
+
+app.listen(3000);
